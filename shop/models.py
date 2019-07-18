addcd286d59a2ff007fdddddc710f652add8be7c
Viktor <viktop.orkush@gmail.com>
2019-07-18 17:09:20 +0300

    create shop app add models create templates for category and product

--- shop/models.py
+++ shop/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.urls import reverse
+
 
 class Category(models.Model):
     name = models.CharField(max_length=250, unique=True)
@@ -11,8 +13,11 @@ class Category(models.Model):
         verbose_name = 'category'
         verbose_name_plural = 'categories'
 
-        def __str__(self):
-            return '{}'.format(self.name)
+    def get_url(self):
+        return reverse('shop:product_by_category', args=[self.slug])
+
+    def __str__(self):
+        return '{}'.format(self.name)
 
 class Product (models.Model):
     name = models.CharField(max_length=250, unique=True)
@@ -30,6 +35,10 @@ class Product (models.Model):
         ordering = ('name',)
         verbose_name = 'product'
         verbose_name_plural = 'products'
+
+    # function to get usr in templates of current product
+    def get_url(self):
+        return reverse('shop:ProdCatDetail', args=[self.category.slug, self.slug])
 
     def __str__(self):
         return '{}'.format(self.name)
--- shop/views.py
+++ shop/views.py
@@ -10,8 +10,15 @@ def allProdCat(request, c_slug=None):
     c_page = None
     products = None
     if c_slug!=None:
-        c_page = get_object_or_404(Category, c_slug=c_slug)
+        c_page = get_object_or_404(Category, slug=c_slug)
         products = Product.objects.filter(category=c_page, available=True)
     else:
         products = Product.objects.all().filter(available=True)
     return render(request,'shop/category.html', {'category':c_page, 'products':products})
+
+def ProdCatDetail(request, c_slug, product_slug):
+    try:
+        product = Product.objects.get(category__slug=c_slug, slug=product_slug)
+    except Exception as e:
+        raise e
+    return render(request, 'shop/product.html', {'product':product})
