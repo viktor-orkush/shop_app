db680ec31c14799bc8d4f715532aa39d6775a6d1
viktor.orkush <viktor.orkush@gmail.com>
2019-08-15 22:01:48 +0300

    commit on other computer when reinstall windows

--- cart/urls.py
+++ cart/urls.py
@@ -5,7 +5,7 @@ app_name = 'cart'
 
 urlpatterns = [
     path('add/<int:product_id>/', views.add_cart, name='add_cart'),
-    path('delete/<int:product_id>/', views.delete_cart, name='delete_cart'),
+    path('remove/<int:product_id>/', views.remove_cart, name='remove_cart'),
     path('trash/<int:product_id>', views.trash_cart, name='trash_cart'),
     path('', views.cart_detail, name='cart_detail'),
 ]
--- cart/views.py
+++ cart/views.py
@@ -1,7 +1,7 @@
 from builtins import dict
 
 from django.core.exceptions import ObjectDoesNotExist
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Cart, CartItem
 from shop.models import Product
 
@@ -41,10 +41,11 @@ def add_cart(request, product_id):
     return redirect('cart:cart_detail')
 
 
-def delete_cart(request, product_id):
-    product = Product.objects.get(id=product_id)
-    cart_item = CartItem.objects.get(product=product)
-    if cart_item.quantity > 0:
+def remove_cart(request, product_id):
+    cart = Cart.objects.get(cart_id=_cart_id(request))
+    product = get_object_or_404(Product, id=product_id)
+    cart_item = CartItem.objects.get(product=product, cart=cart)
+    if cart_item.quantity > 1:
         cart_item.quantity -= 1
         cart_item.save()
     else:
@@ -53,8 +54,9 @@ def delete_cart(request, product_id):
 
 
 def trash_cart(request, product_id):
-    product = Product.objects.get(id=product_id)
-    cart_item = CartItem.objects.get(product=product)
+    cart = Cart.objects.get(cart_id=_cart_id(request))
+    product = get_object_or_404(Product, id=product_id)
+    cart_item = CartItem.objects.get(product=product, cart=cart)
     cart_item.delete()
     return redirect('cart:cart_detail')
 
